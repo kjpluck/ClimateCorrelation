63456401b04ae5e05c85d8962a880f836be2b688
Kevin Pluck <kevinjpluck@gmail.com>
2017-11-22 23:33:46 +0000

    3 columns zooming out looking up

--- src/ClimateCorrelationApp.cpp
+++ src/ClimateCorrelationApp.cpp
@@ -13,16 +13,15 @@ using namespace std;
 
 class ClimateCorrelationApp : public App {
   public:
-	void setup() override;
+	  void setup() override;
 	void update() override;
 	void draw() override;
+	CameraPersp		mCamera;
 private:
 	map<string, float> annualCo2Data1850;
 	map<string, float> monthlyCo2Data1958;
 	map<string, float> monthlyTemperatureData1880;
 	map<string, float> monthlySeaLevelData1880;
-	int x;
-	int y;
 	float temperature;
 	float seaLevel;
 	gl::TextureRef		mTextTexture;
@@ -43,7 +42,11 @@ private:
 	vector<BarrelSegment> seaLevelBarrel;
 	vector<BarrelSegment> arcticSeaIceBarrel;
 	vector<BarrelSegment> antarcticSeaIceBarrel;
+
+	float barrelRadius;
+	void drawBarrel(vector<BarrelSegment>, float x, float y, float z, float scale);
 };
+
 
 struct Date {
 	int year;
@@ -71,21 +74,36 @@ Date GetDayFromFractionalYear(float time) {
 
 void ClimateCorrelationApp::setup()
 {
+	gl::enableDepthRead();
+	gl::enableDepthWrite();
+
+
+	mCamera.setPerspective(50.0f, getWindowAspectRatio(), 0.1f, 1000.0f);
+	//mCamera.lookAt(vec3(0, 15, 30), vec3(0, 0, 0));
+
 	Date foo = GetDayFromFractionalYear(2017.4959);
 	annualCo2Data1850 = DataLoader::loadAnnualCo2Data1850();
 	monthlyCo2Data1958 = DataLoader::loadMonthlyCo2Data1958();
 	monthlyTemperatureData1880 = DataLoader::loadMonthlyTemperature1880();
 	monthlySeaLevelData1880 = DataLoader::loadMonthlySeaLevelData1880();
 	mFont = Font("Arial", 32);
+
+	barrelRadius = 10;
 }
 
 
 
 void ClimateCorrelationApp::update()
 {
-	float time = getElapsedSeconds() * 2 + 1880;
+	float elapsedTime = getElapsedFrames() / 30.0f;
+
+	float zoom = elapsedTime * 0.02f + 1.0f;
+	mCamera.lookAt(vec3(0.0f, 10.0f, 30.0f * zoom), vec3(0.0f, elapsedTime/10.0, 0.0f));
+
+
+	float time = elapsedTime * 2 + 1850;
 	Date theDate = GetDayFromFractionalYear(time);
-	float angleInBarrel = (time - theDate.year) * 2 * M_PI;
+	float angleInBarrel = (theDate.month / 12.0f) * 2 * M_PI;
 
 	char yearMonth[8];
 	sprintf(yearMonth, "%04d-%02d", theDate.year, theDate.month);
@@ -100,12 +118,31 @@ void ClimateCorrelationApp::update()
 		co2CylinderHeight = annualCo2Data1850[to_string(theDate.year)];
 	else {
 		if (yearMonthHasChanged) {
-			BarrelSegment barrelSegment = { angleInBarrel, monthlyCo2Data1958[yearMonth], 1, Color(1,1,1) };
-			co2Barrel.push_back(barrelSegment);
+			float co2ppm = monthlyCo2Data1958[yearMonth];
+			if (co2ppm > 0) {
+				BarrelSegment barrelSegment = { angleInBarrel, co2ppm, 1, Color(1,1,1) };
+				co2Barrel.push_back(barrelSegment);
+			}
+			else {
+				// Missing data in CO2 readings are set to -99
+				// so copy the last CO2 reading and push onto end of vector
+				if (co2Barrel.size() > 0) {
+					BarrelSegment lastBarrelSegment = co2Barrel.back();
+
+					BarrelSegment copyOfLast;
+					copyOfLast.height = lastBarrelSegment.height;
+					copyOfLast.angle = angleInBarrel;
+					copyOfLast.lineWeight = lastBarrelSegment.lineWeight;
+					copyOfLast.color = lastBarrelSegment.color;
+
+					co2Barrel.push_back(copyOfLast);
+				}
+			}
+
 		}
 	}
-
-	if (yearMonthHasChanged) {
+	
+	if (theDate.year >= 1880 && yearMonthHasChanged) {
 		BarrelSegment temperatureSegment = { angleInBarrel, monthlyTemperatureData1880[yearMonth], 1, Color(1,1,1) };
 		temperatureBarrel.push_back(temperatureSegment);
 
@@ -113,11 +150,9 @@ void ClimateCorrelationApp::update()
 		seaLevelBarrel.push_back(seaLevelSegment);
 	}
 		
-	x = getWindowWidth() / 2;
-	y = getWindowHeight();
 
 
-	string txt = to_string(theDate.year) + ": " + to_string(co2CylinderHeight);
+	string txt = to_string(theDate.year) + "/" + to_string(theDate.month);
 	TextBox tbox = TextBox().font(mFont).text(txt);
 	mTextTexture = gl::Texture2d::create(tbox.render());
 
@@ -130,11 +165,55 @@ void ClimateCorrelationApp::update()
 
 void ClimateCorrelationApp::draw()
 {
-	gl::clear(Color(0, 0, 0));
-	gl::drawLine(vec2(x, y), vec2(x, y- co2CylinderHeight));
-	gl::drawLine(vec2(x / 2, y), vec2(x / 2, y - (temperature * 100 + 300)));
-	gl::drawLine(vec2(x*1.5, y), vec2(x*1.5, y - (seaLevel + 300)));
-	gl::draw(mTextTexture, vec2(x, y - co2CylinderHeight));
+	gl::color(1, 1, 1);
+	gl::clear(Color(0, 0.0f, 0.15f));
+	gl::setMatrices(mCamera);
+
+	float co2Scale = 0.4;
+	float co2OffSet = -315 * co2Scale;
+
+	cinder::geom::Cylinder co2Cylinder = cinder::geom::Cylinder();
+	co2Cylinder.set(vec3(0, co2OffSet, 0), vec3(0, co2CylinderHeight * co2Scale + co2OffSet, 0));
+	co2Cylinder.radius(10);
+
+	gl::draw(co2Cylinder);
+
+	float temperatureScale = 37.0;
+	float temperatureOffSet = 0.8 * temperatureScale - 30.0f;
+
+	float seaLevelScale = 0.2;
+	float seaLevelOffSet = 184.5 * seaLevelScale - 15.0f;
+
+	drawBarrel(temperatureBarrel, 25, temperatureOffSet, 0, temperatureScale);
+	drawBarrel(seaLevelBarrel, -25, seaLevelOffSet, 0, seaLevelScale);
+	drawBarrel(co2Barrel, 0, co2OffSet, 0, co2Scale);
+
+	gl::setMatricesWindow(getWindowSize());
+	gl::draw(mTextTexture, vec2(10,10));
 }
 
-CINDER_APP( ClimateCorrelationApp, RendererGl )
+void ClimateCorrelationApp::drawBarrel(vector<BarrelSegment> barrel, float x, float y, float z, float scale) {
+
+	BarrelSegment lastBarrelSegment;
+	bool first = true;
+	float rotationAngle = getElapsedFrames() * 0.005;
+	for (auto &barrelSegment : barrel) {
+		if (first) {
+			lastBarrelSegment = barrelSegment;
+			first = false;
+			continue;
+		}
+		float x1 = cos(lastBarrelSegment.angle + rotationAngle) * barrelRadius;
+		float z1 = sin(lastBarrelSegment.angle + rotationAngle) * barrelRadius;
+		float y1 = lastBarrelSegment.height * scale;
+		float x2 = cos(barrelSegment.angle + rotationAngle) * barrelRadius;
+		float z2 = sin(barrelSegment.angle + rotationAngle) * barrelRadius;
+		float y2 = barrelSegment.height * scale;
+
+		gl::drawLine(vec3(x1 + x, y1 + y, z1 + z), vec3(x2 + x, y2 + y, z2 + z));
+		lastBarrelSegment = barrelSegment;
+	}
+}
+
+CINDER_APP(ClimateCorrelationApp, RendererGl, [&](App::Settings *settings) {
+	settings->setWindowSize(1920, 1080); })
